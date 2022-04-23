1ca8d27348553da0d8cd72b70433f2e0534ed702
Bassist_Zero <bassist.zero@icloud.com>
2022-04-23 20:51:50 +0300

    Initial Commit

---
+++ M.U.S.E./ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M.U.S.E.
+//
+//  Created by Bassist_Zero on 4/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
