b9e4f617c1af88b840b3885e2332873cf65efe40
Bassist_Zero <bassist.zero@icloud.com>
2023-02-21 16:20:12 +0300

    update UI to use new views

--- M.U.S.E./Controllers/ProgressionViewController.swift
+++ M.U.S.E./Controllers/ProgressionViewController.swift
@@ -4,11 +4,8 @@ final class ProgressionViewController: UIViewController {
 
     // MARK: - Private Outlets
 
-    @IBOutlet private weak var notePickerView: NotePickerView!
-    @IBOutlet private weak var progressionType: UISegmentedControl!
-    @IBOutlet private weak var progressionTypeColor: UISegmentedControl!
-    @IBOutlet private weak var progressionVersion: UISegmentedControl!
-    @IBOutlet private weak var ProgressionTextView: UITextView!
+    @IBOutlet private weak var progressionConfigurationView: ProgressionConfigurationView!
+    @IBOutlet private weak var progressionTextView: UITextView!
     @IBOutlet private weak var polyphonySwitch: UISwitch!
 
     // MARK: - Private Properties
@@ -30,20 +27,6 @@ extension ProgressionViewController {
 
 }
 
-// MARK: - IBActions
-
-extension ProgressionViewController {
-
-    @IBAction func GenerateProgression(_ sender: UIButton) {
-        configureProgressionView()
-    }
-
-    @IBAction func playProgression(_ sender: UIButton) {
-        playProgression()
-    }
-
-}
-
 // MARK: - Configuration
 
 private extension ProgressionViewController {
@@ -55,74 +38,26 @@ private extension ProgressionViewController {
     }
 
     func configureProgressionView() {
-        let type = configureType()
-        let version = configureVersion()
-        let keyNote = configureKeyNote()
-
-        progression = Progression(root: keyNote, type: type, version: version)
-        ProgressionTextView.text = progression?.description
-    }
-
-    func configureKeyNote() -> Note {
-        let value = notePickerView.selectedNoteValue
-        let octave = notePickerView.selectedOctave
-
-        let keyNote = Note(octave: octave, value: value)
-
-        return keyNote
-    }
-
-    func configureVersion() -> ProgressionVersion {
-        switch progressionVersion.selectedSegmentIndex {
-        case 0:
-            return .oneThreeFourFive
-        case 1:
-            return .oneTwoThreeFour
-        case 2:
-            return .oneSixFourFive
-        case 3:
-            return .random
-        default:
-            return .random
-        }
-    }
-
-    //swiftlint:disable cyclomatic_complexity
-    func configureType() -> ScaleType {
-        switch progressionType.selectedSegmentIndex {
-        case 0:
-            switch progressionTypeColor.selectedSegmentIndex {
-            case 0:
-                return .minorNatural
-            case 1:
-                return .minorHarmonic
-            case 2:
-                return .minorMelodic
-            default:
-                break
-            }
-
-        case 1:
-            switch progressionTypeColor.selectedSegmentIndex {
-            case 0:
-                return .majorNatural
-            case 1:
-                return .majorHarmonic
-            case 2:
-                return .majorMelodic
-            default:
-                break
-            }
-
-        default:
-            break
-        }
-
-        return .minorNatural
+        progression = progressionConfigurationView.progression
+        progressionTextView.text = progression?.description
     }
 
     func configureProgressionPlayer() {
         progressionPlayer = ProgressionPlayer()
+    }
+
+}
+
+// MARK: - IBActions
+
+extension ProgressionViewController {
+
+    @IBAction func GenerateProgression(_ sender: UIButton) {
+        configureProgressionView()
+    }
+
+    @IBAction func playProgression(_ sender: UIButton) {
+        playProgression()
     }
 
 }
