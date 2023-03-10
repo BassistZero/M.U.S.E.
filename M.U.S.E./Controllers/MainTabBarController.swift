9f99860eccf185f90bc8ee8594894e3a72289ecc
Bassist_Zero <bassist.zero@icloud.com>
2023-03-10 19:32:20 +0300

    IBF-3 Delete different feature tab

--- M.U.S.E./Controllers/MainTabBarController.swift
+++ M.U.S.E./Controllers/MainTabBarController.swift
@@ -18,18 +18,14 @@ private extension MainTabBarController {
     func setupInitialState() {
         let chordTab = ChordViewController()
         let progressionTab = ProgressionViewController()
-        let savedProgressionsTab = SavedProgressionsController()
 
         progressionTab.tabBarItem.title = L10n.TabBar.Item.progression
         progressionTab.tabBarItem.image = UIImage(systemName: "music.quarternote.3")
 
-        savedProgressionsTab.tabBarItem.title = L10n.TabBar.Item.savedProgressions
-        savedProgressionsTab.tabBarItem.image = UIImage(systemName: "music.note.list")
-
         chordTab.tabBarItem.title = L10n.TabBar.Item.chord
         chordTab.tabBarItem.image = UIImage(systemName: "music.note")
 
-        viewControllers = [progressionTab, savedProgressionsTab, chordTab]
+        viewControllers = [progressionTab, chordTab]
 
     }
 
