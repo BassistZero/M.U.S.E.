e8a91e71e9131acc2dbf98d0a570c2dce9adbdac
Bassist_Zero <bassist.zero@icloud.com>
2023-04-19 18:08:56 +0300

    IBF-4 Update configuration tab

--- M.U.S.E./Flows/SongBuilderTab/Detail/Setup/SetupTableViewCell.swift
+++ M.U.S.E./Flows/SongBuilderTab/Detail/Setup/SetupTableViewCell.swift
@@ -1,15 +1,34 @@
 import UIKit
 
-final class SetupTableViewCell: UITableViewCell, SongBuilderTableViewCell {
+final class SetupTableViewCell: UITableViewCell, SongBuilderPart {
 
     // MARK: - Private Outlets
 
     @IBOutlet private weak var configurationView: ProgressionConfigurationView!
+    @IBOutlet private weak var tempoNameLabel: UILabel!
+    @IBOutlet private weak var tempoLabel: UILabel!
+    @IBOutlet private weak var tempoStepper: UIStepper!
+    @IBOutlet private weak var sizeNameLabel: UILabel!
+    @IBOutlet private weak var upTimeButton: UIButton!
+    @IBOutlet private weak var downTimeButton: UIButton!
 
     // MARK: - SongBuilderTableViewCell
 
     var song: Song?
     var rootNavigationController: UINavigationController?
+
+    // MARK: - Private Properties
+
+    private var upTimeValue: Int = 0 {
+        didSet {
+            song?.time?.up = upTimeValue
+        }
+    }
+    private var downTimeValue: BeatLength = .quarter {
+        didSet {
+            song?.time?.down = downTimeValue
+        }
+    }
 
     // MARK: - UIViewController
 
@@ -25,11 +44,75 @@ final class SetupTableViewCell: UITableViewCell, SongBuilderTableViewCell {
 private extension SetupTableViewCell {
 
     func setupInitialState() {
+        configurationView.hideProgressionVersions()
+
         updateSong()
 
         configurationView.didTapped = {
             self.updateSong()
         }
+
+        configureButtons()
+        configureSteppers()
+        configureLabels()
+    }
+
+    func configureButtons() {
+        upTimeButton.showsMenuAsPrimaryAction = true
+
+        var upActions: [UIAction] = []
+
+        Array(2...15).forEach { up in
+            upActions.append(.init(title: "\(up)", handler: { _ in
+                self.upTimeButton.setTitle("\(up)", for: .normal)
+                self.upTimeValue = up
+            }))
+        }
+
+        let upMenu = UIMenu(children: upActions)
+
+        upTimeButton.menu = upMenu
+
+        let upValue = 4
+
+        upTimeButton.setTitle("\(upValue)", for: .normal)
+        upTimeValue = upValue
+
+        downTimeButton.showsMenuAsPrimaryAction = true
+
+        var downActions: [UIAction] = []
+
+        [BeatLength.quarter, BeatLength.eighth, BeatLength.sixteenth].forEach { beatLength in
+            downActions.append(.init(title: beatLength.description, handler: { _ in
+                self.downTimeButton.setTitle(beatLength.description, for: .normal)
+                self.downTimeValue = beatLength
+            }))
+        }
+
+        let downMenu = UIMenu(children: downActions)
+
+        downTimeButton.menu = downMenu
+
+        let downValue = BeatLength.quarter
+
+        downTimeButton.setTitle(downValue.description, for: .normal)
+        downTimeValue = downValue
+    }
+
+    func configureLabels() {
+        tempoNameLabel.text = L10n.SongBuilder.Detail.Configuration.tempoName
+        tempoLabel.text = "\(Int(tempoStepper.value))"
+
+        sizeNameLabel.text = L10n.SongBuilder.Detail.Configuration.timeName
+    }
+
+    func configureSteppers() {
+        tempoStepper.value = 120
+        tempoStepper.minimumValue = 40
+        tempoStepper.maximumValue = 240
+        tempoStepper.stepValue = 1
+
+        tempoStepper.addTarget(self, action: #selector(updateTempo), for: .valueChanged)
     }
 
     func updateSong() {
@@ -40,6 +123,17 @@ private extension SetupTableViewCell {
         else { return }
 
         song?.key = key
+    }
+
+}
+
+// MARK: - Private Actions
+
+@objc
+private extension SetupTableViewCell {
+
+    func updateTempo() {
+        tempoLabel.text = "\(Int(tempoStepper.value))"
     }
 
 }
